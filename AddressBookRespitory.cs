60553b6583e385df160262ad1de6707383754403
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-01 19:24:32 +0530

    initial commit

---
+++ AddressBookRespitory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ADO_AddressBook.ADO_AddressBook
+{
+    class AddressBookRespitory
+    {
+        //Give path for Database Connection
+        public static string connection = @"Server=.;Database=Address_Book_Service_DB;Trusted_Connection=True;";
+        //Represents a connection to Sql Server Database
+        SqlConnection sqlConnection = new SqlConnection(connection);
+    }
+}
