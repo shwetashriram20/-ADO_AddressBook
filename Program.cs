58bcce481a6ec521768c41f52a163866e89c7ebb
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-02 17:00:01 +0530

    Insert into table

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book ADO!");
+        }
+    }
+}
