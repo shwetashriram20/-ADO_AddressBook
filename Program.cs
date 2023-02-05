b1f7e23d5e777c2b39ffe21eb55fc83ba69650df
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-05 11:58:11 +0530

    greeting msg

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ADO_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address Book ADO!");
+        }
+    }
+}
